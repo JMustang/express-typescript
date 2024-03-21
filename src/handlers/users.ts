b74ddc614dc60f3e83d9ce0b2c4e71ea49c52711
Junior <eecfredes@gmail.com>
2024-03-20 21:25:04 -0300

    :rocket: query testes

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { query, Request, Response } from "express";
 import { CreateUserDto } from "../dtos/CreateUser.dto";
+import { CreateUserQueryParams } from "../types/query-params";
 
 export function getUsers(req: Request, res: Response) {
   res.send([]);
@@ -9,6 +10,10 @@ export function getUsersById(req: Request, res: Response) {
   res.send({});
 }
 
-export function createUser(req: Request<{}, {}, CreateUserDto>, res: Response) {
+export function createUser(
+  req: Request<{}, {}, CreateUserDto, CreateUserQueryParams>,
+  res: Response
+) {
   req;
+  req.query.loginAfterCreate;
 }
